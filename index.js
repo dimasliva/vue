2932f90dc416947cdec361944a985e9734bc24f3
Pipa PlohoiAnimator <janzerikYT@yandex.ru>
2021-10-03 06:20:15 +0500

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 app.get("/", (req, res) => {
-  res.end("<h1>Hello</h1>");
+  res.end("<h1>Hello world</h1>");
 });
 
 app.get("/about", (req, res) => {
